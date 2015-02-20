baf7c70c24330f7424b0101d61f2c113dce6ce87
an4 <ad12461@my.bristol.ac.uk>
2015-02-20 20:32:35 +0000

    Routing for Cards

--- controller/main.js
+++ controller/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-angular.module('myApp', ['ngMaterial']);
+var app = angular.module('myApp', ['ngMaterial', 'ngRoute']);
+
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/cards', {
+        templateUrl: 'view/cards.html',
+        controller: 'CardsController'
+      }).
+      when('/cards/:cardId', {
+        templateUrl: 'view/card.html',
+        controller: 'CardController'
+      }).
+      otherwise({
+        redirectTo: '/cards'
+      });
+  }]);
