b9a9b3cb7a5c5d976ab1f38d9cdda00179a91f5d
an4 <ad12461@my.bristol.ac.uk>
2015-02-27 16:41:12 +0000

    Big card design

--- controller/card_ctrl.js
+++ controller/card_ctrl.js
@@ -2,9 +2,13 @@
 
 var app = angular.module('myApp');
 
-app.controller('card', ['$routeParams', '$scope', '$http',
-    function($routeParams, $scope, $http) {
+app.controller('card', ['$routeParams', '$scope', '$http', '$location',
+    function($routeParams, $scope, $http, $location) {
         $http.get('data/' + $routeParams.cardId + '.json').success(function(data) {
-        $scope.card = data;
-    });
+            $scope.card = data;
+        });
+
+        $scope.back = function() {
+            $location.path("/cards");
+        };
 }]);
