404d155487f4fad62f2f83c0171777e5e242c3f1
an4 <ad12461@my.bristol.ac.uk>
2015-03-03 17:49:14 +0000

    Svg + transition

--- controller/card_ctrl.js
+++ controller/card_ctrl.js
@@ -2,10 +2,22 @@
 
 var app = angular.module('myApp');
 
-app.controller('card', ['$routeParams', '$scope', '$http', '$location',
-    function($routeParams, $scope, $http, $location) {
+app.controller('card', ['$routeParams', '$scope', '$http', '$location', '$timeout',
+    function($routeParams, $scope, $http, $location, $timeout) {
         $http.get('data/' + $routeParams.cardId + '.json').success(function(data) {
             $scope.card = data;
+            var images = data.mediaURLs;
+            var length = images.length;
+            var i = 0;
+            var transition = function() {
+                if(i >= length) {
+                    i = 0;
+                }
+                $('.image-transition').attr("src", images[i]);
+                i++;
+                $timeout(transition, 10000);
+            }
+            transition();
         });
 
         $scope.back = function() {
