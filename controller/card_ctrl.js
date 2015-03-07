4f15169f2605e1652b730fa0f7866573edd78d98
an4 <ad12461@my.bristol.ac.uk>
2015-03-07 12:54:15 +0000

    Transition

--- controller/card_ctrl.js
+++ controller/card_ctrl.js
@@ -4,23 +4,30 @@ var app = angular.module('myApp');
 
 app.controller('card', ['$routeParams', '$scope', '$http', '$location', '$timeout',
     function($routeParams, $scope, $http, $location, $timeout) {
+        var images;
         $http.get('data/' + $routeParams.cardId + '.json').success(function(data) {
             $scope.card = data;
-            var images = data.mediaURLs;
+            images = data.mediaURLs;
+            $('.image-transition').attr("src", images[0]);
             var length = images.length;
             var i = 0;
             var transition = function() {
+                i++;
                 if(i >= length) {
                     i = 0;
                 }
-                $('.image-transition').attr("src", images[i]);
-                i++;
+                $('.image-transition').fadeOut(500, function() {
+                    $(this).attr("src", images[i]).bind('onreadystatechange load', function() {
+                        if (this.complete) $(this).fadeIn(500);
+                    });
+                });
                 $timeout(transition, 10000);
             }
             transition();
         });
 
         $scope.back = function() {
+            images = [];
             $location.path("/cards");
         };
 }]);
