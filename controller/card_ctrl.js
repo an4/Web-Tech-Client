0b5500bc020a1af5d5a286eea984a6bfcfc31f36
an4 <ad12461@my.bristol.ac.uk>
2015-03-07 21:05:31 +0000

    Fixed lag in transition

--- controller/card_ctrl.js
+++ controller/card_ctrl.js
@@ -8,21 +8,32 @@ app.controller('card', ['$routeParams', '$scope', '$http', '$location', '$timeou
         $http.get('data/' + $routeParams.cardId + '.json').success(function(data) {
             $scope.card = data;
             images = data.mediaURLs;
-            $('.image-transition').attr("src", images[0]);
-            var length = images.length;
+
+            // Preload images
             var i = 0;
+            var length = images.length;
+            for(i=0; i<length; i++) {
+                var img = $('<img class="image-transition"></img>');
+                img.attr("src", images[i]);
+                img.attr("id", i);
+                img.hide();
+                $('.card-media').append(img);
+            }
+
+            i = 0;
             var transition = function() {
+                var old = $("#" + i);
+                old.hide(500);
                 i++;
                 if(i >= length) {
                     i = 0;
                 }
-                $('.image-transition').fadeOut(500, function() {
-                    $(this).attr("src", images[i]).bind('onreadystatechange load', function() {
-                        if (this.complete) $(this).fadeIn(500);
-                    });
-                });
+                var current = $("#" + i);
+                current.show(500);
+
                 $timeout(transition, 10000);
             }
+
             transition();
         });
 
